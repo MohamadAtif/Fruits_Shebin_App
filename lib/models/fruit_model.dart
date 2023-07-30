1149bb40ab92fbc0f9dd52f8f359872114e4f8c1
MohamadAtif <137002849+MohamadAtif@users.noreply.github.com>
2023-07-30 18:47:55 +0300

    create typr adapter for fruit model

---
+++ lib/models/fruit_model.dart
@@ -0,0 +1,17 @@
+import 'package:hive/hive.dart';
+part 'fruit_model.g.dart';
+
+@HiveType(typeId: 0)
+class FruitModel extends HiveObject{
+  @HiveField(0)
+  final String image;
+  @HiveField(1)
+  final String title;
+  @HiveField(2)
+  final double price;
+  @HiveField(3)
+  final int color;
+
+  FruitModel({required this.color, required this.image,required this.title,required this.price});
+  
+}
